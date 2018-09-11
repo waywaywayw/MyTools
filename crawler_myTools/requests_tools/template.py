3d14b08310e39e58950c9a9c491017408200d045
waywaywayw <906415310@qq.com>
2018-09-11 23:18:12 +0800

    Initial commit

---
+++ crawler_myTools/requests_tools/template.py
@@ -0,0 +1,3 @@
+headers = {'Connection': 'Keep-Alive'
+           # ,'Host': 'zhannei.baidu.com'
+           ,'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063'}
