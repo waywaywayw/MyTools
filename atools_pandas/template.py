0798de2ece565be5c06dae571c33c530168180dc
Way <906415310@qq.com>
2018-10-13 16:20:54 +0800

    增加template

--- atools_pandas/template.py
+++ atools_pandas/template.py
@@ -7,6 +7,8 @@ import os
 import re
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from atools_pandas.my_pandas import drop_rows
 
 
 def process_content(data):
@@ -18,17 +20,18 @@ def process_content(data):
 
 if __name__ == "__main__":
     # 初始化DataFrame
-    # data_pd = pd.read_excel(os.path.join(""))
+    # input_path = os.path.join("")
+    # data_pd = pd.read_excel(input_path)
     # data_pd = data_pd.sample(frac=0.1)
     print('初始化DataFrame完毕')
 
     # 开始处理DataFrame
     # 1. 构造完整的DataFrame
-
     # 2. 针对某些项逐个处理
     # data_pd['content'] = data_pd['content'].map(lambda x: process_content(x))
     print('处理DataFrame完毕')
 
     # 处理完毕，保存DataFrame
-    # data_pd.to_csv()
+    # train_data_pd, test_data_pd = train_test_split(data_pd, test_size=0.1, random_state=2018)
+    # train_data_pd.to_csv()
     print('保存DataFrame完毕')
